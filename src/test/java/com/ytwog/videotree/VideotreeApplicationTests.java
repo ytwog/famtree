1f8884250b3b01e3a4f951ff52833529cd247bb0
Апанасенко Вячеслав <vapanasenko@blogic.ru>
2021-11-28 02:52:43 +0700

    FT-1: Repo init

---
+++ src/test/java/com/ytwog/videotree/VideotreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ytwog.videotree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideotreeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
